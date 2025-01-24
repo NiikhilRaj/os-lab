1521184b2d369d5095c0e79cd7898ce832850cd0
Your Name <nikhilraj13733@gmail.com>
2025-01-24 17:05:59 +0530

    day5 3 wird codes

--- day5/selectionSort.bash
+++ day5/selectionSort.bash
@@ -5,15 +5,15 @@ selection_sort() {
     n=${#arr[@]}
     
     for ((i = 0; i < n-1; i++)); do
-        min_idx=$i
+        min_index=$i
         for ((j = i+1; j < n; j++)); do
-            if (( arr[j] < arr[min_idx] )); then
-                min_idx=$j
+            if (( arr[j] < arr[min_index] )); then
+                min_index=$j
             fi
         done
         temp=${arr[i]}
-        arr[i]=${arr[min_idx]}
-        arr[min_idx]=$temp
+        arr[i]=${arr[min_index]}
+        arr[min_index]=$temp
     done
 
     echo "${arr[@]}"
@@ -23,5 +23,4 @@ read -p "Enter the elements of the array:" -a array
 
 sorted_array=($(selection_sort "${array[@]}"))
 
-# Print the sorted array
 echo "Sorted array: ${sorted_array[@]}"

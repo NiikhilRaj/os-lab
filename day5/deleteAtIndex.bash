104a1ae9ea1034c457627520dc36711523e6fea3
Your Name <nikhilraj13733@gmail.com>
2025-01-24 22:52:31 +0530

    day5 3 wird codes

--- day5/deleteAtIndex.bash
+++ day5/deleteAtIndex.bash
@@ -8,15 +8,12 @@ delete_at_index() {
     echo "${array[@]}"
 }
 
-# Read array from user
 echo "Enter the elements of the array (space-separated):"
 read -a array
 
-# Read index from user
 echo "Enter the index to delete:"
 read index
 
-# Check if index is valid
 if [ $index -ge 0 ] && [ $index -lt ${#array[@]} ]; then
     new_array=$(delete_at_index $index "${array[@]}")
     echo "New array: $new_array"
